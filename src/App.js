81fc7cfc94987a878420497acbec45e87a737170
mknyambura <kariuki.mercie@gmail.com>
2022-12-23 15:06:08 +0300

    update commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import { Blog, Features, Footer, Header, Possibility, WhatUIUX } from './containers';
+import { Brand, CTA, Navbar } from './components';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>Modern UI/UX</h1>
+    <div className='App'>
+      <div className='gradient__background'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatUIUX />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
---
+++ src/containers/whatUIUX/WhatUIUX.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatUIUX.css'
+
+function WhatUIUX() {
+  return (
+    <div>WhatUIUX</div>
+  )
+}
+
+export default WhatUIUX
