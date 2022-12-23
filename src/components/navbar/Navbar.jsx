98060807b1416448dfefc983d9813a16e5b97f4c
mknyambura <kariuki.mercie@gmail.com>
2022-12-23 23:43:48 +0300

    update commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import './navbar.css'
 const Menu = () => (
   <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#wgpt3">What is GPT3?</a></p>
+    <p><a href="#whatgpt3">What is GPT3?</a></p>
     <p><a href="#possibility">OpenAI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
