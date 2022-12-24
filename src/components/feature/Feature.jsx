0fe02ba848a250600aea57b5d03ab21424071912
mknyambura <kariuki.mercie@gmail.com>
2022-12-24 05:02:54 +0300

    update commit

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './feature.css'
 
-function Feature() {
+function Feature({title, text}) {
   return (
-    <div>Feature</div>
+    <div className='features-container-feature'>
+        <div className="features-container-feature-title">
+          <div/>
+          <h1 >{title}</h1>
+        </div>
+        <div className="features-container-feature-text">
+          <p>{text}</p>
+        </div>
+    </div>
   )
 }
 
