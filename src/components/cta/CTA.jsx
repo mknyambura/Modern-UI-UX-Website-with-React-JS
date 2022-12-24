e6e6a648942e0b512e826e23e1ddc948c985966a
mknyambura <kariuki.mercie@gmail.com>
2022-12-24 11:48:20 +0300

    update commit

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 function CTA() {
   return (
-    <div>CTA</div>
+    <div className='cta'>
+      <div className="cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="cta-button">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
