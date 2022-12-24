562eea025d940214a5a72b73f202102250c71f10
mknyambura <kariuki.mercie@gmail.com>
2022-12-24 06:03:49 +0300

    initial commit

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './possibility.css'
 
+import possibilityImage from '../../assets/possibility.png'
+
 function Possibility() {
   return (
-    <div>Possibility</div>
+    <div className='possibilit section-padding' id='possiility'>
+      <div className="possibility-image">
+        <img src={possibilityImage} alt="" />
+      </div>
+      <div className="possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
   )
 }
 
