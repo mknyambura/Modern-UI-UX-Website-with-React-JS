06022643da0e38e26041bc5c23188e4603ebb97e
mknyambura <kariuki.mercie@gmail.com>
2022-12-24 05:56:38 +0300

    update commit

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,7 +3,7 @@ import './feature.css'
 
 function Feature({title, text}) {
   return (
-    <div className='features-container-feature'>
+    <div id="features-container-feature" className='features-container-feature'>
         <div className="features-container-feature-title">
           <div/>
           <h1 >{title}</h1>
